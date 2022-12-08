e114a8792df1a265f44747a2b6c5dbe1e0eb1d03
ebo275 <115588483+ebo275@users.noreply.github.com>
2022-12-08 11:37:26 -0800

    shows changed to anime

--- animefront/src/App.js
+++ animefront/src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-import Show from './components/show.js'
+import Anime from './components/Anime.js'
 import Add from './components/Add.js'
 
 const App = ()=> {
 
-  const [shows, setShows] = useState([])
+  const [anime, setAnimes] = useState([])
   
   //READING 1
-  const getShows = () => {
-    axios.get('http://localhost:3000/shows').then((response) => setShows(response.data), (err) => console.log(err)).catch((error)=> console.log(error))
+  const getAnimes = () => {
+    axios.get('http://localhost:3000/animes').then((response) => setAnimes(response.data), (err) => console.log(err)).catch((error)=> console.log(error))
   }
 
   //ADDING
   const handleCreate = (data) => {
-    axios.post('http://localhost:3000/shows', data).then((response) => {
+    axios.post('http://localhost:3000/animes', data).then((response) => {
       console.log(response)
-      setShows([...shows, response.data])
+      setShows([...animes, response.data])
     })
   }
 
@@ -26,29 +26,29 @@ const App = ()=> {
 
 
   //DELETE
-const handleDelete = (deletedShow) => {
-  axios.delete('http://localhost:3000/shows/' + deletedShow._id).then((response) => {
-    let newShows = shows.filter((show) => {
-      return show._id !== deletedShow._id
+const handleDelete = (deletedAnime) => {
+  axios.delete('http://localhost:3000/shows/' + deletedAnime._id).then((response) => {
+    let newAnimes = animes.filter((anime) => {
+      return anime._id !== deletedAnime._id
     })
-    setShows(newShows)
+    setShows(newAnimes)
   })
 }
 
   //READING 2
   useEffect(() => {
-    getShows()
+    getAnimes()
   }, [])
 
   return (
     <>
     <h1>Anime Wonderland</h1>
     <Add handleCreate={handleCreate}/>
-    {shows.map((show) => {
+    {animes.map((anime) => {
         return (
           <>
-            <Show show={show}/>
-            <button onClick={() => {handleDelete(show)}} value={show._id}>Delete</button>
+            <Anime anime={anime}/>
+            <button onClick={() => {handleDelete(anime)}} value={anime._id}>Delete</button>
           </>
         )})}
     </>
