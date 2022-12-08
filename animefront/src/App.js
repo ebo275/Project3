c26aafb9817cb7c050b949f9764d43a7f3ce9a37
ebo275 <115588483+ebo275@users.noreply.github.com>
2022-12-08 10:52:49 -0800

    changed show to anime

--- animefront/src/App.js
+++ animefront/src/App.js
@@ -1,36 +1,36 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-import Show from './components/show.js'
+import Anime from './components/Anime.js'
 import Add from './components/Add.js'
 
 const App = ()=> {
 
-  const [shows, setShows] = useState([])
+  const [animes, setAnimes] = useState([])
   
-  const getShows = () => {
-    axios.get('http://localhost:300/shows').then((response) => setShows(response.data), (err) => console.log(err)).catch((error)=> console.log(error))
+  const getAnimes = () => {
+    axios.get('http://localhost:300/anime').then((response) => setShows(response.data), (err) => console.log(err)).catch((error)=> console.log(error))
   }
 
   const handleCreate = (data) => {
-    axios.post('http://localhost:300/shows', data).then((response) => {
+    axios.post('http://localhost:300/anime', data).then((response) => {
       console.log(response)
-      setShows([...shows, response.data])
+      setAnimes([...animes, response.data])
     })
   }
 
   useEffect(() => {
-    getShows()
+    getAnimes()
   }, [])
 
   return (
     <>
     <h1>Anime Wonderland</h1>
     <Add handleCreate={handleCreate}/>
-    {shows.map((show) => {
+    {animes.map((anime) => {
         return (
           <>
-            <Show show={show}/>
+            <Anime anime={anime}/>
           </>
         )})}
     </>
